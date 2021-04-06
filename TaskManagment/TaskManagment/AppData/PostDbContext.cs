9b00a44f609261bf51ac48ac2881b79db70dbf6e
Harun Kaknjo <harun.kaknjo2018@size.ba>
2021-04-06 07:52:13 -0700

    Configured TaskDbContext class

---
+++ TaskManagment/TaskManagment/AppData/PostDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagment.Models;
+
+namespace TaskManagment.AppData
+{
+    public class PostDbContext : DbContext
+    {
+        public PostDbContext(DbContextOptions<PostDbContext> options)
+           : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Post> Posts { get; set; }
+    }
+}
