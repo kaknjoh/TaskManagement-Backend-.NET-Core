fd9f65d9ace5a2963c423a793075770e902234fe
Harun Kaknjo <harun.kaknjo2018@size.ba>
2021-04-06 07:53:50 -0700

    DAL with implemented PostRepository for CRUD operations

---
+++ TaskManagment/TaskManagment/DAL/IPostRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagment.Models;
+
+namespace TaskManagment.DAL
+{
+    public interface IPostRepository
+    {
+        Task<IEnumerable<Post>> getAllPostsAsync();
+
+        Task SavePost(Post post);
+
+        Task<Post> GetPostByIdAsync(int id);
+
+        Task EditPostAsync(Post post);
+
+        Task DeletePostAsync(Post post);
+    }
+}
---
+++ TaskManagment/TaskManagment/DAL/PostRepository.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagment.AppData;
+using TaskManagment.Models;
+
+namespace TaskManagment.DAL
+{
+    public class PostRepository : IPostRepository
+    {
+
+        PostDbContext _context;
+       public PostRepository(PostDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task DeletePostAsync(Post post)
+        {
+            _context.Posts.Remove(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditPostAsync(Post post)
+        {
+            _context.Posts.Update(post);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Post>> getAllPostsAsync()
+        {
+            return await _context.Posts.ToListAsync();
+        }
+
+        public async Task<Post> GetPostByIdAsync(int id)
+        {
+            return await _context.Posts.SingleOrDefaultAsync(c => c.PostId == id);
+        }
+
+        public async Task SavePost(Post post)
+        {
+            _context.Posts.Add(post);
+            await _context.SaveChangesAsync();
+
+        }
+
+
+        
+    }
+}
