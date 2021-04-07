ae0d236a8bfe630a7425f7835c7f93124896d8a9
Harun Kaknjo <harun.kaknjo2018@size.ba>
2021-04-07 14:51:58 -0700

    Repository for AssignedUser

---
+++ TaskManagment/TaskManagment/DAL/AssignedUserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagment.AppData;
+using TaskManagment.Models;
+
+namespace TaskManagment.DAL
+{
+    public class AssignedUserRepository : IAssignedUserRepository
+    {
+        TaskManagementDbContext _context;
+
+        public AssignedUserRepository(TaskManagementDbContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task DeleteAssignedUserAsync(AssignedUser assignedUser)
+        {
+            _context.AssignedUsers.Remove(assignedUser);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task EditAssignedUserAsync(AssignedUser assignedUser)
+        {
+            _context.AssignedUsers.Update(assignedUser);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<AssignedUser>> GetAllAssignedUsersAsync()
+        {
+            return await _context.AssignedUsers.ToListAsync();
+        }
+
+        public async Task<AssignedUser> GetAssignedUserByIdAsync(int id)
+        {
+            return await _context.AssignedUsers.SingleOrDefaultAsync(c => c.AssignedUserId == id);
+        }
+
+        public async Task SaveAssignedUserAsync(AssignedUser assignedUser)
+        {
+            _context.AssignedUsers.Add(assignedUser);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ TaskManagment/TaskManagment/DAL/IAssignedUserRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagment.Models;
+
+namespace TaskManagment.DAL
+{
+    public interface IAssignedUserRepository
+    {
+        public Task<IEnumerable<AssignedUser>> GetAllAssignedUsersAsync();
+
+        public Task SaveAssignedUserAsync(AssignedUser assignedUser);
+
+        public Task<AssignedUser> GetAssignedUserByIdAsync(int id);
+
+        public Task EditAssignedUserAsync(AssignedUser assignedUser);
+
+        public Task DeleteAssignedUserAsync(AssignedUser assignedUser);
+
+
+
+    }
+}
