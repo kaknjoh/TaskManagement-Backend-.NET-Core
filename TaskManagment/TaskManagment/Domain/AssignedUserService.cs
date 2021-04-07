1ef5665f2797e25b4588a07aaf922621f7790d54
Harun Kaknjo <harun.kaknjo2018@size.ba>
2021-04-07 15:57:04 -0700

    Services and Controller with API endpoint that enable CRUD operations on AssignedUser table

---
+++ TaskManagment/TaskManagment/Domain/AssignedUserService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagment.DAL;
+using TaskManagment.Models;
+
+namespace TaskManagment.Domain
+{
+    public class AssignedUserService : IAssignedUserService
+    {
+        private readonly IAssignedUserRepository _assignedUserRepository;
+
+        public AssignedUserService(IAssignedUserRepository assignedUserRepository)
+        {
+            _assignedUserRepository = assignedUserRepository;
+        }
+
+        public Task DeleteAssignedUserAsync(AssignedUser assignedUser)
+        {
+            return  _assignedUserRepository.DeleteAssignedUserAsync(assignedUser);
+        }
+
+        public Task EditAssignedUserAsync(AssignedUser assignedUserInDb, AssignedUser assignedUser)
+        {
+           assignedUserInDb.FirstName = assignedUser.FirstName;
+           assignedUserInDb.LastName = assignedUser.LastName;
+           return _assignedUserRepository.EditAssignedUserAsync(assignedUserInDb);
+        }
+
+        public Task<IEnumerable<AssignedUser>> GetAllAssignedUsersAsync()
+        {
+            return _assignedUserRepository.GetAllAssignedUsersAsync();
+        }
+
+        public Task<AssignedUser> GetAssignedUserByIdAsync(int id)
+        {
+            return _assignedUserRepository.GetAssignedUserByIdAsync(id);
+        }
+
+        public Task SaveAssignedUserAsync(AssignedUser assignedUser)
+        {
+            return _assignedUserRepository.SaveAssignedUserAsync(assignedUser);
+        }
+    }
+}
+
---
+++ TaskManagment/TaskManagment/Domain/IAssignedUserService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagment.Models;
+
+namespace TaskManagment.Domain
+{
+    public interface IAssignedUserService
+    {
+        public Task<IEnumerable<AssignedUser>> GetAllAssignedUsersAsync();
+
+        public Task SaveAssignedUserAsync(AssignedUser assignedUser);
+
+        public Task<AssignedUser> GetAssignedUserByIdAsync(int id);
+
+        public Task EditAssignedUserAsync(AssignedUser assignedUserInDb, AssignedUser assignedUser);
+
+        public Task DeleteAssignedUserAsync(AssignedUser assignedUser);
+    }
+}
